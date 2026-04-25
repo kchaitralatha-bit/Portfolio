5ac5e16ffa74896a2f63779da6f3b5ab7a9bab0d
K CHAITRA LATHA <kchaitralatha@gmail.com>
2026-04-25 13:26:52 +0530

    Add AchievementIcons component with icons export

---
+++ portfolio-chaitra-main/src/components/portfolio/AchievementIcons.tsx
@@ -0,0 +1,3 @@
+import { TrophyIcon, LightbulbIcon, DnaIcon, GraduationCapIcon, StarIcon } from "lucide-react";
+
+export { TrophyIcon, LightbulbIcon, DnaIcon as DNAIcon, GraduationCapIcon, StarIcon };
