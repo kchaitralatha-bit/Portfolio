cc25c5c251227c55034a6327a14b57f60328e549
K CHAITRA LATHA <kchaitralatha@gmail.com>
2026-04-25 13:25:13 +0530

    Delete portfolio-chaitra-main/src/components/portfolio/C:\Users\chait\Downloads\portfolio-chaitra-main\portfolio-chaitra-main\src\components\portfolio\AchievementIcons.tsx

--- portfolio-chaitra-main/src/components/portfolio/C:\Users\chait\Downloads\portfolio-chaitra-main\portfolio-chaitra-main\src\components\portfolio\AchievementIcons.tsx
+++
@@ -1,3 +0,0 @@
-import { TrophyIcon, LightbulbIcon, DnaIcon, GraduationCapIcon, StarIcon } from "lucide-react";
-
-export { TrophyIcon, LightbulbIcon, DnaIcon as DNAIcon, GraduationCapIcon, StarIcon };
